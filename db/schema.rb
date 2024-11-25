f6eb54e8e52cb8714b95d36d1f7ab14b9ff7aae9
Ellis Whaley <eewhaley@icloud.com>
2024-11-25 17:38:33 +0000

    made a recipes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_171400) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_173422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_171400) do
     t.string "name"
     t.string "category"
     t.float "carbon_per_gram"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "instructions", null: false
+    t.string "cuisine", null: false
+    t.string "image_url", null: false
+    t.string "cook_time", null: false
+    t.string "difficulty", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

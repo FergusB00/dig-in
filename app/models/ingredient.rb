dc42126c32e9a927a8d18d40727d372d0f5f0c89
Fergus Buscall <buscallf@gmail.com>
2024-11-26 11:57:16 +0000

    Associations for recipes, recipe ingredients, ingredients, meals and user ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,8 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+  has_many :user_ingredients
+  has_many :users, through: :users
+
+
 end

c307d710c46d18f57a64ba600d45e5d37f59e7b2
Eugenia Leong <eugenialeong@gmail.com>
2024-11-26 18:04:58 +0000

    users and ingredients controllers update

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,11 +6,4 @@ class IngredientsController < ApplicationController
   def show
     @ingredient = Ingredient.find(params[:id])
   end
-
-  def new
-    @ingredient = Ingredient.new
-  end
-
-  def create
-  end
 end

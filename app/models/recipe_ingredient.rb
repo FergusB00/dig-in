fd9b816ffea9182357462da9ddf9f13d3c1baef6
Fergus Buscall <buscallf@gmail.com>
2024-11-27 13:46:49 +0000

    Model method converting to grams

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,7 +3,7 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
 
   after_create :convert_items_to_servings
-  # after_create :convert_weight_to_grams
+  after_create :convert_weight_to_grams
 
   def convert_items_to_servings
     if self.unit == "small" || self.unit == "medium" || self.unit == "large"
@@ -11,8 +11,16 @@ class RecipeIngredient < ApplicationRecord
     end
   end
 
-  # def convert_weight_to_grams
-
-  # end
-
+  def convert_weight_to_grams
+    self.weight_in_grams = case unit.downcase
+                           when "servings" then quantity * 100
+                           when "g" then quantity
+                           when "cloves" then quantity * 10
+                           when "tbsps" then quantity * 15
+                           when "l" then quantity * 1000
+                           when "ml" then quantity * 1
+                           else quantity * 20
+                           end
+    self.save!
+  end
 end

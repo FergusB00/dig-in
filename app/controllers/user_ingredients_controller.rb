24282a89f0adaccce97df218fd27c39cce2c5e76
Eugenia Leong <eugenialeong@gmail.com>
2024-11-27 15:12:12 +0000

    useringredients controller and add items modal update

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,16 +1,19 @@
 class UserIngredientsController < ApplicationController
   def create
-    @ingredient = Ingredient.find(params[:ingredient_id])
-    @new_user_ingredient = user_ingredient.new(new_user_ingredient_params)
-  end
-
-  def new
-    @user_ingredient = UserIngredient.new
+    @user_ingredient = UserIngredient.new(user_ingredient_params)
+    @user_ingredient.user = current_user
+    if @user_ingredient.save
+      redirect_to profile_path, notice: "Ingredient successfully added!"
+    else
+      p @user_ingredient.errors.messages
+      redirect_to profile_path, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def new_user_ingredient_params
-    params.require(:user_ingredients).permit(:quantity, :unit, :price, :expiry) #ingredient name?
+  def user_ingredient_params
+    #check if need to add name to user_ingredients schema
+    params.require(:user_ingredient).permit(:ingredient_id, :quantity, :unit, :price_in_pence, :expiry_date)
   end
 end

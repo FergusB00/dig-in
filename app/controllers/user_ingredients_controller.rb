128379b702f3a8e6c00360f459e6b946fd128559
Eugenia Leong <eugenialeong@gmail.com>
2024-11-27 12:32:26 +0000

    UI update

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,9 +1,12 @@
 class IngredientsController < ApplicationController
-  def index
-    @ingredients = Ingredient.all
+  def create
+    @ingredient = Ingredient.find(params[:ingredient_id])
+    @new_user_ingredient = user_ingredient.new(new_user_ingredient_params)
   end
 
-  def show
-    @ingredient = Ingredient.find(params[:id])
+  private
+
+  def new_user_ingredient_params
+    params.require(:user_ingredients).permit(:quantity, :unit, :price, :expiry) #ingredient name?
   end
 end

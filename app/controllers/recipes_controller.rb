cbead1cb1269086eb552160a6d920d4318cbc807
Fergus Buscall <buscallf@gmail.com>
2024-11-28 17:36:39 +0000

    Filter working and user ingredients added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,22 +2,26 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show]
 
   def index
+    if params[:query].present?
+      p "Query is present"
+      @recipes = Recipe.search_by_name_and_cuisine(params[:query])
+      p @recipes.count
+    else
+      @recipes = Recipe.all
+    end
+
     if params[:available_ingredients_only].present?
+      p "available only please!"
       user_ingredients = current_user.ingredients #change current_user.ingredients to filter user_ingredients by expiry here if needed
       available_recipe_ids = RecipeIngredient.where(ingredient: user_ingredients).pluck(:recipe_id)
 
-      @recipes = Recipe.where(id: available_recipe_ids)
-    else
-      @recipes = Recipe.all
+      @recipes = @recipes.where(id: available_recipe_ids)
     end
 
     # if params[:query].present?
     #   @recipes.search_by_filter(params[:query])
     # end
 
-    if params[:query].present?
-      @recipes.search_by_name_and_cuisine(params[:query])
-    end
 
   end
 

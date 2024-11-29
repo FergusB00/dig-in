554e22ede02992335b7ad5118067e094b33fd998
Eugenia Leong <eugenialeong@gmail.com>
2024-11-29 14:43:10 +0000

    meals controller done. profile dashboard working

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -19,6 +19,7 @@ class MealsController < ApplicationController
     )
 
     if @meal.save
+      update_user_ingredients_qty(@recipe)
       redirect_to profile_path, notice: "#{@meal.recipe.name} Added!"
     else
       p @meal.errors.messages
@@ -35,12 +36,17 @@ class MealsController < ApplicationController
   #carbon for all ingredients in meal, not just user ingredients used in meal
   def calculate_carbon_savings(recipe)
     recipe.recipe_ingredients.sum do |recipeingredient|
+      p recipeingredient
       user_ingredient = current_user.user_ingredients
                                     .where(ingredient: recipeingredient.ingredient)
                                     .where('weight_in_grams >= ?', recipeingredient.weight_in_grams)
                                     .where('expiry_date >= ?', Date.today).order(expiry_date: :desc).first
-      ingredient = recipeingredient.ingredient
-      ingredient.carbon_per_gram * user_ingredient.weight_in_grams
+      if user_ingredient.nil?
+        0
+      else
+        ingredient = recipeingredient.ingredient
+        p ingredient.carbon_per_gram * user_ingredient.weight_in_grams
+      end
     end
   end
 
@@ -60,12 +66,13 @@ class MealsController < ApplicationController
   end
 
   def update_user_ingredients_qty(recipe)
+    p "updating user recipes"
     recipe.recipe_ingredients.each do |recipeingredient|
       user_ingredient = current_user.user_ingredients
                                     .where(ingredient: recipeingredient.ingredient)
                                     .where('weight_in_grams >= ?', recipeingredient.weight_in_grams)
                                     .where('expiry_date >= ?', Date.today).order(expiry_date: :desc).first
-      user_ingredient.update(weight_in_grams: user_ingredient.weight_in_grams - recipeingredient.weight_in_grams)
+      user_ingredient&.update(weight_in_grams: user_ingredient.weight_in_grams - recipeingredient.weight_in_grams)
     end
   end
 end

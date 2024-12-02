53ef037feb6d088b256b76c86da1617d322b3b2a
Fergus Buscall <buscallf@gmail.com>
2024-12-02 13:05:05 +0000

    Scanner added to add items modal

--- app/javascript/controllers/barcode_controller.js
+++ app/javascript/controllers/barcode_controller.js
@@ -1,26 +1,37 @@
 import { Controller } from "@hotwired/stimulus";
-// import { Html5QrcodeScanner } from "html5-qrcode";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 export default class extends Controller {
   static targets = ["reader"]
   connect() {
     console.log("Barcode scanner connected");
 
+    const onScanSuccess = (decodedText, decodedResult) => {
+      // handle the scanned code as you like, for example:
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+    }
+
+    const onScanFailure = (error) => {
+      // handle scan failure, usually better to ignore and keep scanning.
+      // for example:
+      console.warn(`Code scan error = ${error}`);
+    }
+
 
     let html5QrcodeScanner = new Html5QrcodeScanner(
       this.readerTarget.id,
-      { fps: 10, qrbox: { width: 250, height: 250 } },
-      /* verbose= */ true
-    );
+      { fps: 1, qrbox: {width: 250, height: 250} },
+        /* verbose= */ true
+      );
 
-    html5QrcodeScanner.render(this.onScanSuccess, this.onScanFailure);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
   }
 
-  onScanSuccess = (decodedText, decodedResult) => {
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-  };
+  // onScanSuccess(decodedText, decodedResult) {
+  //   console.log(`Code matched = ${decodedText}`, decodedResult);
+  // };
 
-  onScanFailure = (error) => {
-    console.warn(`Code scan error = ${error}`);
-  };
+  // onScanFailure(error) {
+  //   console.warn(`Code scan error = ${error}`);
+  // };
 }

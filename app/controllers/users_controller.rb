701810e1122c4dff2a73154f0d279d1a63227a54
Fergus Buscall <buscallf@gmail.com>
2024-12-02 09:07:48 +0000

    Trying to order ingredients by sell by

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
     @recipes = current_user.recipes
-    @ingredients = current_user.user_ingredients.includes(:ingredient)
+    @ingredients = current_user.user_ingredients.includes(:ingredient).order(created_at: :desc)
     @user_ingredient = UserIngredient.new
     @meals = current_user.meals
     @total_carbon = @meals.map { |meal| meal.carbon_saving }.sum / 1000

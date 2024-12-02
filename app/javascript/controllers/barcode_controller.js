5f48c22cbc6ed298b5b8dd830b8c91cc39f3fb0f
Fergus Buscall <buscallf@gmail.com>
2024-12-02 14:55:41 +0000

    ZXing

--- app/javascript/controllers/barcode_controller.js
+++ app/javascript/controllers/barcode_controller.js
@@ -1,37 +1,72 @@
 import { Controller } from "@hotwired/stimulus";
-import { Html5QrcodeScanner } from "html5-qrcode";
+import { BrowserQRCodeReader } from '@zxing/library';
+// import { BrowserQRCodeReader } from '@zxing/browser';
 
 export default class extends Controller {
   static targets = ["reader"]
   connect() {
     console.log("Barcode scanner connected");
 
-    const onScanSuccess = (decodedText, decodedResult) => {
-      // handle the scanned code as you like, for example:
-      console.log(`Code matched = ${decodedText}`, decodedResult);
-    }
+    this.codeReader = new BrowserQRCodeReader();
 
-    const onScanFailure = (error) => {
-      // handle scan failure, usually better to ignore and keep scanning.
-      // for example:
-      console.warn(`Code scan error = ${error}`);
-    }
+      // console.log()
+      // codeReader.listVideoInputDevices().then(videoInputResult => {
+      //   console.log(videoInputResult[0].deviceId);
+        // result is video input device
+      this.codeReader.decodeFromVideoDevice(undefined, 'video')
+        .then(result => {
+          console.log(result);
 
 
-    let html5QrcodeScanner = new Html5QrcodeScanner(
-      this.readerTarget.id,
-      { fps: 1, qrbox: {width: 250, height: 250} },
-        /* verbose= */ true
-      );
+    })
 
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    console.log(this.codeReader);
+
+      // })
+
+    // const videoInputDevices = ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
+    // const videoInputDevices = new ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
+
+    // choose your media device (webcam, frontal camera, back camera, etc.)
+    // const selectedDeviceId = videoInputDevices[0].deviceId;
+
+    // console.log(`Started decode from camera with id ${selectedDeviceId}`);
+
+    // const previewElem = document.querySelector('#test-area-qr-code-webcam > video');
+
+    // you can use the controls to stop() the scan or switchTorch() if available
+    // const controls = codeReader.decodeFromVideoDevice(undefined, 'video')
+    // .then(result => {
+    //   console.log(result);
+
+    // })
+    // => {
+    //   // use the result and error values to choose your actions
+    //   // you can also use controls API in this scope like the controls
+    //   // returned from the method.
+    // });
+
+    // stops scanning after 20 seconds
+    // setTimeout(() => controls.stop(), 20000);
+
+    // const onScanSuccess = (decodedText, decodedResult) => {
+    //   // handle the scanned code as you like, for example:
+    //   console.log(`Code matched = ${decodedText}`, decodedResult);
+    // }
+
+    // const onScanFailure = (error) => {
+    //   // handle scan failure, usually better to ignore and keep scanning.
+    //   // for example:
+    //   console.warn(`Code scan error = ${error}`);
+    // }
+
+
+    // let html5QrcodeScanner = new Html5QrcodeScanner(
+    //   this.readerTarget.id,
+    //   { fps: 1, qrbox: {width: 250, height: 250} },
+    //     /* verbose= */ true
+    //   );
+
+    // html5QrcodeScanner.render(onScanSuccess, onScanFailure);
   }
-
-  // onScanSuccess(decodedText, decodedResult) {
-  //   console.log(`Code matched = ${decodedText}`, decodedResult);
-  // };
-
-  // onScanFailure(error) {
-  //   console.warn(`Code scan error = ${error}`);
-  // };
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 # pin "html5-qrcode", to: "https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js" # @2.3.8
+pin "@zxing/library", to: "https://ga.jspm.io/npm:@zxing/library@0.20.0/esm/index.js" # @0.21.3
+pin "ts-custom-error" # @3.3.1

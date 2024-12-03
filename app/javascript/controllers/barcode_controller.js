5d8a3d454f6552adfaff994f163720046b724814
Fergus Buscall <buscallf@gmail.com>
2024-12-03 15:05:27 +0000

    Barcode up and running

--- app/javascript/controllers/barcode_controller.js
+++ app/javascript/controllers/barcode_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static values = {
+    api: String
+  }
+
   connect() {
     console.log("Connected from barcode controller")
     this.selectedDeviceId = null;
     this.codeReader = new ZXing.BrowserMultiFormatReader()
     console.log('ZXing code reader initialized')
+    console.log(this.apiValue)
   }
 
 
@@ -18,7 +23,7 @@ export default class extends Controller {
       console.log('Capture recording')
       if (result) {
         console.log(result)
-        this.getBarcodeData
+        this.getBarcodeData(result)
         this.codeReader.reset()
       }
       if (err && !(err instanceof ZXing.NotFoundException)) {
@@ -35,12 +40,26 @@ export default class extends Controller {
 
   getBarcodeData(result) {
     const barcode = result.text
-    const apiKey = ENV['BARCODE_API_KEY']
+    const apiKey = this.apiValue;
+    console.log(apiKey)
     const url = `https://api.barcodelookup.com/v3/products?barcode=${barcode}&formatted=y&key=${apiKey}`;
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      const ingredientName = data.products[0].title;
+      console.log(ingredientName);
 
+      const size = data.products[0].size;
+      const sizeFormat = /(\d+)([a-zA-Z]+)/;
+      const match = size.match(sizeFormat);
+      const ingredientWeight = match[1];
+      const ingredientUnit = match[2];
 
+      console.log(ingredientWeight)
+      console.log(ingredientUnit)
+  })
   }
-
   // .catch((err) => {
   //   console.error(err)
   // })
